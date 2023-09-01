b9c382c9e227c7d113b4ef4d250cbdf82f12050c
zhuuuuchok <83700463+zhuuuuchok@users.noreply.github.com>
2023-09-01 12:21:55 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -8,7 +8,7 @@ if os.access(file_path, os.F_OK) == True:
 with open('input.txt' , 'r') as f:
     text = f.read()
 
-sentences = re.split(r'[.!?]+[\n]', text)
+sentences = re.split(r'[.!?]', text)
 number_of_sentences=len(sentences)
 
 #используем регулярное выражение, чтобы искать слова в тексте, не учитывая знаки препинания и перенос строки
@@ -21,6 +21,6 @@ print(sentences)
 
 # создаем файлик для записи вывода
 with open('output.txt', 'w') as f:
-    f.write (f'Количество предложений в тексте: {number_of_sentences}\n')
+    f.write (f'Количество предложений в тексте: {number_of_sentences-1}\n')
     f.write (f'Количество слов в тексте: {number_of_words}\n')
     f.write (f'Количество слов в тексте, длиннее 5 символов: {num_of_long_words}\n')
